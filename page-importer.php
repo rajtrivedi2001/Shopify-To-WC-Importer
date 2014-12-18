1788d13bf08c5ee370596148171c9ca0b6575d85
Tech No Freaky <varunsridharan23@gmail.com>
2014-12-18 08:45:15 +0530

    Added Product Importer Class
    Called Product Importer Class.

--- page-importer.php
+++ page-importer.php
@@ -4,11 +4,13 @@ define('base_dir',get_template_directory().'/shopify_importer/');
 require(base_dir.'importer_functions.php');
 require(base_dir.'config.php');
 require(base_dir.'json_gen.php');
+require(base_dir.'importer.php');
 
 base_setup();
 $start = timer(); 
 
-new shopify_json();
+$json_gen = new shopify_json();
+new product_importer();
  
   
  
@@ -16,4 +18,4 @@ new shopify_json();
 $total_time = round((timer() - $start), 4);
 text('Page generated in '.$total_time.' seconds.');
 saveLOG();
-?> 
+?>
