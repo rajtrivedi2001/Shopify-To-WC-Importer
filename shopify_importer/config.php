0d8c3c8a64c1845d4bb261f3ac4f8b044ee7f310
Tech No Freaky <varunsridharan23@gmail.com>
2014-12-18 08:47:14 +0530

    Added Few Keys
    Added The Blow Keys. & Removed Some Spaces
    products_base_key 
    collection_base_key
    product_category_term
    product_tag_term
    product_vendor_term
    product_type_term
    product_status
    product_post_type
    product_author

--- shopify_importer/config.php
+++ shopify_importer/config.php
@@ -4,18 +4,27 @@
 =========================================*/
 define('instant_update',true);
 define('dataFOLDER',base_dir.'data/');
+define('products_base_key','products');
+define('collection_base_key','custom_collections');
 
+define('product_category_term','product_cat');
+define('product_tag_term','product_tag');
+define('product_vendor_term','Brands');
+define('product_type_term','Product Type');
 
+define('product_status','publish');
+define('product_post_type','product');
+define('product_author',1);
 
 
 /*=======================================
  * Shopify Settings
 ========================================*/
-define('site_url','https://cakesquarechennai.myshopify.com/');
-define('api_key','1e7dd451c59733590549c701766cde45');
-define('api_pass','2b479ec35353de1d046096ae717f5a50');
-define('product_fetch_limit','25');
-define('collections_fetch_limit','25');
+define('site_url','https://pink-panthers.myshopify.com/');
+define('api_key','1761b3a189540bcbcc74dbe04a31a13f');
+define('api_pass','0436bd25f14dd0ac015cad6634d46760');
+define('product_fetch_limit','250');
+define('collections_fetch_limit','250');
 
 
 /*======================================
@@ -36,7 +45,5 @@ define('product_count_file','productsCOUNT.json');
 define('product_json_file','product_json{$count}.json');
 define('collections_count_file','collectionsCOUNT.json');
 define('collections_json_file','collections_json{$count}.json');
-$messageLogs = array(); 
-
-
+$messageLogs = array();
 ?>
